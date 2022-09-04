3c78c95bfe8d2d2e9fb04102bd097cd3af6b844b
Victor <victorgpj25@gmail.com>
2022-09-04 18:05:07 -0300

    app: errorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
+import "express-async-errors"
 import dotenv from "dotenv"
 import cors from "cors"
 dotenv.config()
 
 import router from "./routes/index"
+import errorHandler from "./middlewares/errorHandler"
 
 
 const app = express()
@@ -11,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
     console.log("server running on port " + process.env.PORT)
