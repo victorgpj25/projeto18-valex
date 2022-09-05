99b4f09beeafb92968d5dd7df12fe5e3edaca2a8
Victor <victorgpj25@gmail.com>
2022-09-05 10:26:34 -0300

    unblockCard: route

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -4,14 +4,16 @@ import {
     createCard, 
     activateCard, 
     displayBalance,
-    blockCard
+    blockCard,
+    unblockCard
 } from "../controllers/cardController"
 import { verifyApiKey } from "../middlewares/authMiddleware"
 import {
     validateCreateCardData, 
     validateActivateCardReqBody, 
     validateDisplayBalanceReqBody,
-    validateBlockCardReqBody
+    validateBlockCardReqBody,
+    validateUnblockCardReqBody
 } from "../middlewares/cardMiddleware"
 
 const cardRouter = express.Router()
@@ -20,5 +22,7 @@ cardRouter.post("/card/create", verifyApiKey, validateCreateCardData, createCard
 cardRouter.post("/card/activate", validateActivateCardReqBody, activateCard)
 cardRouter.post("/card/balance", validateDisplayBalanceReqBody, displayBalance)
 cardRouter.post("/card/block", validateBlockCardReqBody, blockCard)
+cardRouter.post("/card/unblock", validateUnblockCardReqBody, unblockCard)
+
 
 export default cardRouter
