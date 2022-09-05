dc0f32f79e8a6b78a68da4f1f4a2b184bf2d18cb
Victor <victorgpj25@gmail.com>
2022-09-04 23:15:05 -0300

    displayBalance: route

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,12 +1,21 @@
 import express from "express"
 
-import { createCard, activateCard } from "../controllers/cardController"
+import {
+    createCard, 
+    activateCard, 
+    displayBalance
+} from "../controllers/cardController"
 import { verifyApiKey } from "../middlewares/authMiddleware"
-import { validateCreateCardData, validateActivateCardReqBody } from "../middlewares/cardMiddleware"
+import {
+    validateCreateCardData, 
+    validateActivateCardReqBody, 
+    validateDisplayBalanceReqBody
+} from "../middlewares/cardMiddleware"
 
 const cardRouter = express.Router()
 
 cardRouter.post("/card/create", verifyApiKey, validateCreateCardData, createCard)
 cardRouter.post("/card/activate", validateActivateCardReqBody, activateCard)
+cardRouter.post("/card/balance", validateDisplayBalanceReqBody, displayBalance)
 
 export default cardRouter
