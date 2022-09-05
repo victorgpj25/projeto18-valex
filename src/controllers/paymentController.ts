6e3210894cb3c5419f172346de2e54a674a4fba3
Victor <victorgpj25@gmail.com>
2022-09-05 11:21:55 -0300

    registerPayment: controller

---
+++ src/controllers/paymentController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+import * as paymentService from "../services/paymentService"
+
+export async function registerPayment(req: Request, res: Response) {
+    const { cardId, password, businessId, amount }: {cardId: number, password: string, businessId: number, amount: number } = req.body
+
+    await paymentService.registerPayment(Number(cardId), password, Number(businessId), Number(amount))
+
+    res.sendStatus(200)
+}
