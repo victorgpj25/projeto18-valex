cc890382bff2fbc418d31f72f1de03e8ccc20e76
Victor <victorgpj25@gmail.com>
2022-09-05 09:33:31 -0300

    blockCard: route

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -3,13 +3,15 @@ import express from "express"
 import {
     createCard, 
     activateCard, 
-    displayBalance
+    displayBalance,
+    blockCard
 } from "../controllers/cardController"
 import { verifyApiKey } from "../middlewares/authMiddleware"
 import {
     validateCreateCardData, 
     validateActivateCardReqBody, 
-    validateDisplayBalanceReqBody
+    validateDisplayBalanceReqBody,
+    validateBlockCardReqBody
 } from "../middlewares/cardMiddleware"
 
 const cardRouter = express.Router()
@@ -17,5 +19,6 @@ const cardRouter = express.Router()
 cardRouter.post("/card/create", verifyApiKey, validateCreateCardData, createCard)
 cardRouter.post("/card/activate", validateActivateCardReqBody, activateCard)
 cardRouter.post("/card/balance", validateDisplayBalanceReqBody, displayBalance)
+cardRouter.post("/card/block", validateBlockCardReqBody, blockCard)
 
 export default cardRouter
