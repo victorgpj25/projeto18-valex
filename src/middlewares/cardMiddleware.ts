7763218ea5ef72934674a8c1bff717a0a4f5ab3f
Victor <victorgpj25@gmail.com>
2022-09-04 21:40:14 -0300

    activateCard: request validations

--- src/middlewares/cardMiddleware.ts
+++ src/middlewares/cardMiddleware.ts
@@ -1,11 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
-import { createCardSchema } from "../schemas/cardSchema"
+import { createCardSchema, activateCardSchema } from "../schemas/cardSchema"
 
-export async function validateCreateCardData(req: Request, res: Response, next: NextFunction) {
+export function validateCreateCardData(req: Request, res: Response, next: NextFunction) {
     const validation = createCardSchema.validate(req.body)
     if (validation.error) {
         res.status(422).send({ErrorMessage: "Card creation failed due to " + validation.error})
+        return
+    }
+
+    next();
+}
+
+export function validateActivateCardReqBody(req: Request, res: Response, next: NextFunction) {
+    const validation = activateCardSchema.validate(req.body)
+    if (validation.error) {
+        res.status(422).send({ErrorMessage: "Card activation failed due to " + validation.error})
         return
     }
 
--- src/schemas/cardSchema.ts
+++ src/schemas/cardSchema.ts
@@ -4,8 +4,8 @@ import joi from "joi"
 const createCardSchema = joi.object({
     employeeId: joi.number().required()
         .messages({
-            "any.required": "EmployeeId is required",
-            "number.base": "Sent employeeId is not valid"
+            "any.required": "Employee id is required",
+            "number.base": "Sent employee id is not valid"
         }),
     type: joi.string().valid("groceries", "restaurants", "transport", "education", "health").required()
         .messages({
@@ -15,4 +15,25 @@ const createCardSchema = joi.object({
         })
 })
 
-export { createCardSchema }
+const activateCardSchema = joi.object({
+    id: joi.number().required()
+        .messages({
+            "any.required": "card id is required",
+            "number.base": "Sent card id is not valid"
+        }),
+
+    securityCode: joi.string().pattern(/^[0-9]{3}$/).required()
+        .messages({
+            "any.required": "Card type is required",
+            "string.base": "Sent security code is not valid",
+            "string.pattern.base": "Security codes must be 3 numerical digits long"
+        }),
+    password: joi.string().pattern(/^[0-9]{4}$/).required()
+        .messages({
+            "any.required": "Password is required",
+            "string.base": "Sent password is not valid",
+            "string.pattern.base": "Passwords must be 4 numerical digits long"
+        })
+})
+
+export { createCardSchema, activateCardSchema }
