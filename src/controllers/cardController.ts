6525b3872e4793ec2bd824d6b8b1cf615d98bd18
Victor <victorgpj25@gmail.com>
2022-09-04 21:45:43 -0300

    activateCard: controller

--- src/controllers/cardController.ts
+++ src/controllers/cardController.ts
@@ -14,3 +14,12 @@ export async function createCard(req: Request, res: Response) {
 
     res.sendStatus(201)
 }
+
+export async function activateCard(req: Request, res: Response) {
+    const { id, securityCode, password }: {id: number, securityCode: string, password: string} = req.body
+
+    await cardService.activateCard(Number(id), securityCode, password)
+
+    res.sendStatus(200)
+}
+
